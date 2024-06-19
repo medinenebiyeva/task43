8b325a0316f96e228b5efd9538a5545f4ac6b783
medinenebiyeva <medinenebiyeva1@gmail.com>
2024-06-19 16:49:03 +0400

    nan

---
+++ app.js
@@ -0,0 +1,6 @@
+let text = "Medine shopping etmeyi sevir";
+console.log(text.length);
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.replaceAll("t", "####"))
